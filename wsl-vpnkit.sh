d5d6aa36250bf4719445481b590871255b383a04
patton-git <63215900+patton-git@users.noreply.github.com>
2026-06-05 13:35:54 +0900

    Update wsl-vpnkit.sh

--- wsl-vpnkit.sh
+++ wsl-vpnkit.sh
@@ -92,6 +92,22 @@ sudo systemctl restart wsl-vpnkit
 sudo systemctl status wsl-vpnkit
 
 
+--------------------
 
+sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+sudo apt list --installed | grep -i ansible
 
+mkdir -p ~/.config/pip
+echo -e "[global]\nbreak-system-packages=true" > ~/.config/pip/pip.conf
+
+sudo apt install python3-pip -y
+
+pip install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git --break-system-packages
+
+echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+echo $PATH
 
